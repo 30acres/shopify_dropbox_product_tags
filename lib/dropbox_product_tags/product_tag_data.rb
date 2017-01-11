6b1b36099d943daef287190b56c947d616845d37
Jonny Dalgleish <fighella@gmail.com>
2017-01-11 13:10:17 +1100

    ---

--- lib/dropbox_product_tags/product_tag_data.rb
+++ lib/dropbox_product_tags/product_tag_data.rb
@@ -58,10 +58,14 @@ class ProductTagData
         # @notifier.ping "[Product Data] #{product.inspect}" if ENV['SLACK_CMW_WEBHOOK']
         # binding.pry
         encoded = CSV.parse(row).to_hash.to_json
+        puts '1'
         encoded = row.to_hash.inject({}) { |h, (k, v)| h[k] = v.to_s.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '').valid_encoding? ? v.to_s.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '') : '' ; h }
+        puts '2'
         encoded_more = encoded.to_json
+        puts '3'
         # puts encoded_more
         rd = RawDatum.where(data: encoded_more, client_id: 0, status: 10).first_or_create
+        puts '4'
 
         puts "===================="
         puts "===================="
